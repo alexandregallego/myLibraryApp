1e277b604d8df696021e5ab38050e0cf5051a39f
Alexandre Gallego Mato <alexandre.gallego.mato@gmail.com>
2023-06-04 13:14:34 +0200

    Added detail page + related books functionality

--- mylibrary/book/views.py
+++ mylibrary/book/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
+
+
+def detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    related_books = Book.objects.filter(
+        category=book.category).exclude(pk=pk)
+    return render(request, 'book/detail.html', {
+        'book': book,
+        'related_books': related_books
+    })
--- mylibrary/mylibrary/urls.py
+++ mylibrary/mylibrary/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('books/', include('book.urls')),
     path('contact/', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
