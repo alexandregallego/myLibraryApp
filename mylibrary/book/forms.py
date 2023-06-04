f5d73fa213d1df874ceafb579ce580c609feecc8
Alexandre Gallego Mato <alexandre.gallego.mato@gmail.com>
2023-06-04 19:03:20 +0200

    Add books functionality

---
+++ mylibrary/book/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from .models import Book
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+
+class NewBookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('category', 'name', 'description', 'price', 'image',)
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
--- mylibrary/book/views.py
+++ mylibrary/book/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Book
+from .forms import NewBookForm
 
 # Create your views here.
 
@@ -11,4 +13,25 @@ def detail(request, pk):
     return render(request, 'book/detail.html', {
         'book': book,
         'related_books': related_books
+    })
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewBookForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # If you try to save to the database now, the created_by field is not added. Therefore we will get an error and set commit=False
+            book = form.save(commit=False)
+            book.created_by = request.user
+            book.save()
+
+            return redirect('book:detail', pk=book.id)
+    else:
+        form = NewBookForm()
+
+    return render(request, 'book/form.html', {
+        'form': form,
+        'book': 'New book',
     })
