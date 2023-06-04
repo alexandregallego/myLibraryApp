a09baa48acca906ca14f2ee384936ef95152a096
Alexandre Gallego Mato <alexandre.gallego.mato@gmail.com>
2023-06-04 17:18:31 +0200

    Added signup functionality

--- mylibrary/core/views.py
+++ mylibrary/core/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 # Request parameter is info about your browser, IP address, if it is a GET or POST request. Need to be put in all views that we use.
 
 from book.models import Category, Book
+from .forms import SignupForm
 
 
 def index(request):
@@ -18,3 +19,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    # to check if the user has submitted a form
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()  # the user will be created in the database
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
